cfbbfac0b3d6ad4f3087d3057eb7d5578711f3ce
felix livshits <felix.livshits@shomi.com>
2016-06-08 18:10:56 -0400

    Adding libs and bin to gitignore list. Removing jar files. Adding AndroidLauncher and JSONParser classes. Progress on SampleTest.

---
+++ src/Utilities/AndroidLauncher.java
@@ -0,0 +1,36 @@
+package Utilities;
+
+import java.io.File;
+import java.util.concurrent.TimeUnit;
+
+public class AndroidLauncher {
+
+	private static String sdkPath = "C:/Users/felix.livshits/AppData/Local/Android/Sdk/"; // or for Mac "/Applications/adt-bundle-mac-x86_64-20140702/sdk/";// or for windows D:/Android/adt-bundle-windows-x86_64-20140702/sdk/
+	private static String emulatorPath = sdkPath + "tools" + File.separator + "emulator";
+
+	Process process;
+	/**
+	 * Starts an emulator for the provided AVD name
+	 * 
+	 * @param nameOfAVD
+	 */
+	public void launchEmulator(String nameOfAVD) 
+	{
+		String[] aCommand = new String[] { emulatorPath, "-avd", nameOfAVD };
+		try 
+		{
+			process = new ProcessBuilder(aCommand).start();
+			process.waitFor(20, TimeUnit.SECONDS);
+			System.out.println("Emulator launched successfully!");
+		}	
+		catch (Exception e) 
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public void killEmulator()
+	{
+		process.destroy();
+	}
+}
