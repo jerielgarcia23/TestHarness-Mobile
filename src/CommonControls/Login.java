f1864afef5073770c461e628c759412907b7cc00
felix livshits <felix.livshits@shomi.com>
2016-06-27 17:03:27 -0400

    Network Connectivity tests

--- src/CommonControls/Login.java
+++ src/CommonControls/Login.java
@@ -14,17 +14,6 @@ public class Login {
 
 	public static void login(AppiumDriver driver, String credentials)
 	{
-		
-		try
-		{
-			driver.findElement(By.id("fragment_shomi_notification_dialog_button1")).click();
-			//Alert alert = driver.switchTo().alert();
-		    //alert.getText();
-		    //alert.accept();
-		    DeviceController.enableCommsData(driver);
-		        //Intent intent = new Intent("com.twidroid.SendTweet");
-		}
-		catch(Exception e){}
 		driver.findElement(By.id(LoginPage.performLogin)).click();
 		List<MobileElement> elements = driver.findElements(By.className("android.widget.RadioButton"));
 		for (MobileElement element : elements)
